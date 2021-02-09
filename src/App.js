a43f6e4bc51f34e9041f8cae4e672def5df302d6
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-02-09 14:17:14 +0100

    Testing deploy to GCLOUD

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Sculpture
+          Sculpture is live!
         </a>
       </header>
     </div>
