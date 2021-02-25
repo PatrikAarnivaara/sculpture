8f58b30fb0b67427defb7e4645fc7a8a8ce669b7
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-02-25 12:29:35 +0100

    Refactored and added a search-filter-sort bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
 import Navbar from './UI/Navbar/Navbar';
 import Sculpture from './components/sculpture/Sculpture';
 import Collection from './components/collection/Collection';
 import SculptureContextProvider from './context/SculptureContext';
+import SearchFilterSort from './UI/SearchFilterSort/SearchFilterSort'
+import './App.css';
 
 /* function saveToLocalStorage(selectedSculptures) {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(selectedSculptures));
  } */
 
 function App() {
+
 	return (
 		<div className="app">
 			<SculptureContextProvider>
 				<Router>
 					<Navbar />
+					<SearchFilterSort/>
 					<Switch>
 						<Route path="/" exact component={Sculpture} />
 						<Route path="/collection" exact component={Collection} />
