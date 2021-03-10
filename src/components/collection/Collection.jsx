e86c86516a59d9f32980ad2f24b770d8e4c94c35
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-10 13:13:04 +0100

    removed collectionList- and Item

--- src/components/collection/Collection.jsx
+++ src/components/collection/Collection.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import CollectionList from './CollectionList';
-/* import CheckboxLogic from '../../UI/Checkboxes/CheckboxLogic'; */
 import './Collection.css';
 
 const Collection = () => {
 	return (
 		<div>
-			{/* <div className="collection-header">
-				<h2>collection</h2>
-			</div> */}
-			{/* <CheckboxLogic /> */}
-			<CollectionList />
+			
 		</div>
 	);
 };
